e56ef53675f0cb2b70e16bc384afdcdf00da8546
trepichio <trepichio@gmail.com>
2020-12-27 15:33:37 -0300

    refactored sort for simpler readbility

--- 04-Introdução a Busca e Substituição em JavaScript/Desafio-01.js
+++ 04-Introdução a Busca e Substituição em JavaScript/Desafio-01.js
@@ -37,10 +37,7 @@ for (let i = 0; i < N; i++) {
   const filteredWords = words.filter(w => new RegExp(/^[A-Za-z0-9 ]{1,50}$/).test(w));
 
   const sortedWords = filteredWords.sort((a, b) => {
-    if (a.length > b.length) return -1
-    if (a.length < b.length) return 1
-    if (b.length === a.length) {
-      // console.log(a, b); //para uso local
+    if (b.length - a.length === 0) {
       return a.localeCompare(b, 'en', { sensitivity: 'base' });
     }
     else return b.length - a.length
